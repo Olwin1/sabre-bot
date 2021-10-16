2510a84da82f0af4efae0991ded6a5d3bd00e6fb
Olwin1 <oliver_munn@icloud.com>
2021-10-16 14:28:13 +0100

    Basic Setup & Slash Commands

---
+++ bot.py
@@ -0,0 +1,49 @@
+import discord
+from discord.ext import commands
+
+from discord_slash import SlashCommand, SlashContext
+
+"""
+This Is The Rewrite Version Of Sabre Bot. It Is A Development Version So It Is Buggy.
+The Ultimate Aim It To Create a More Customisable, Compatible and Safe Discord Bot. 
+
+Documentation:
+    http://discordpy.readthedocs.io/en/rewrite/api.html
+Rewrite Commands Documentation:
+    http://discordpy.readthedocs.io/en/rewrite/ext/commands/api.html
+
+"""
+intents = discord.Intents.default()
+bot = commands.Bot(command_prefix="!", intents=intents)
+slash = SlashCommand(bot, sync_commands=True)
+guild_ids = [704255331680911402]
+
+@bot.command(name="test")
+@slash.slash(name="test", guild_ids=guild_ids)
+async def test(ctx: SlashContext):
+    #embed = discord.Embed(title="Embed Test")
+    await ctx.send("success!")
+
+
+
+
+
+
+
+@bot.event
+async def on_ready():
+    """http://discordpy.readthedocs.io/en/rewrite/api.html#discord.on_ready"""
+
+    print(f'\n\nLogged in as: {bot.user.name} - {bot.user.id}\nVersion: {discord.__version__}\n')
+
+    # Changes our bots Playing Status. type=1(streaming) for a standard game you could remove type and url.
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name='Visual Studio Code'))
+    print(f'Successfully logged in and booted...!')
+
+
+
+
+with open("token.key") as f:
+    token = f.read()
+    f.close()
+bot.run(token, bot=True, reconnect=True)
