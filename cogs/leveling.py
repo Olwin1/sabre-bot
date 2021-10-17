65358c5272f2725322ed70deda0997aa9cdb8234
Olwin1 <oliver_munn@icloud.com>
2021-10-17 09:22:05 +0100

    Checks Cache For User & Creates If Not Exist

--- cogs/leveling.py
+++ cogs/leveling.py
@@ -7,15 +7,27 @@ from datetime import datetime
 
 conn = psycopg.connect(dbname="sabre", user="postgres", password="***REMOVED***", host="localhost")
 
+guild_ids = [704255331680911402]
+print("levleing is runnin")
+cooldown = TTLCache(maxsize=1024, ttl=20)
+
+cache = TTLCache(maxsize=100, ttl=10)
 
 
+# This Function Gets The User Data From The Cache. IF It Is Not In The Cache It Fetches It and If It Cannot Be Found It Creates It.
+def getcache_leveling(key, guild_id):
+    selected = cache.get(key)
+    if selected is None:
+      cur = conn.cursor()
+      cur.execute("SELECT guilds,levels,exp FROM users WHERE user_id=%s", (key,))
+      selected = cur.fetchone()
+      if selected is None:
+          cur.execute("INSERT INTO users (user_id, guilds, levels, exp) VALUES (%s, %s, %s, %s)", (key, [guild_id,], [0,], [0,]))
+          conn.commit()
+          cur.execute("SELECT guilds,levels,exp FROM users WHERE user_id=%s", (key,))
+          selected = cur.fetchone()
+    return selected
 
-@cached(cache = TTLCache(maxsize=100, ttl=10))
-def load_data():
-    # run slow data to get all user data
-    load_response = "This Was Cached At " + datetime.strftime(datetime.now(), "%d/%m/%Y %H:%M:%S") + "This Is A TTL Cache And Will Change 10 Seconds After Being Created"
-
-    return load_response
 class Slash(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -23,10 +35,19 @@ class Slash(commands.Cog):
 
 
 
-    @cog_ext.cog_slash(name="cogt")
+    @cog_ext.cog_slash(name="cogt", guild_ids=guild_ids)
     async def _cogt(self, ctx: SlashContext):
-        embed = discord.Embed(title=load_data())
-        await ctx.send(embed=embed)
+        print(2)
+        cache = getcache_leveling(ctx.author.id, ctx.guild.id)
+        print(3)
+        if cache is None:
+            cache = "Empty :("
+        await ctx.send(str(cache))
+        print(type(cache))
+
+
+        
+
 
 def setup(bot):
     bot.add_cog(Slash(bot))
