5f78a63945cbaf7419d3870cf133937e566ac88d
Olwin1 <oliver_munn@icloud.com>
2021-10-18 17:12:34 +0100

    Added Image Gen For Rank

--- cogs/leveling.py
+++ cogs/leveling.py
@@ -5,6 +5,9 @@ import psycopg
 from cachetools import cached, TTLCache, LRUCache
 from datetime import datetime
 import random
+from PIL import Image, ImageDraw, ImageFont
+from discord import File
+import io
 
 conn = psycopg.connect(dbname="sabre", user="postgres", password="jumper123", host="localhost")
 
@@ -46,6 +49,8 @@ def getcache_leveling(key, guild_id):
 class Slash(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        
+    
 
 
     @commands.command(name="cogt")
@@ -54,7 +59,8 @@ class Slash(commands.Cog):
     #@cog_ext.cog_slash(name="cogt", guild_ids=guild_ids)
     #async def _cogt(self, ctx: SlashContext):
 
-        user = getcache_leveling(ctx.author.id, ctx.guild.id)
+        author = ctx.author
+        user = getcache_leveling(author.id, ctx.guild.id)
         user = {"guilds": user[0], "exp": user[1]}
         passed = False
         for i in range(len(user["guilds"])):
@@ -80,13 +86,138 @@ class Slash(commands.Cog):
                 member = {"guild": user["guilds"][i], "level": level, "total_exp": total_exp, "total_exp_next_actual": total_exp_next_actual, "total_exp_next_display": total_exp_next_display, "remaining": remaining, "exp": total_exp_next_display - remaining}
                 #guild, level, total_exp, total_exp_next_actual, total_exp_next_display, remaining
         if not passed:
-            print(cache[ctx.author.id][0])
-            cache[ctx.author.id][0].append(ctx.guild.id)
-            cache[ctx.author.id][1].append(1)
-            print(cache[ctx.author.id][0])
+            print(cache[author.id][0])
+            cache[author.id][0].append(ctx.guild.id)
+            cache[author.id][1].append(1)
+            print(cache[author.id][0])
             member = {"guild": ctx.guild.id, "level": 1, "total_exp": 1, "total_exp_next_actual": 100, "total_exp_next_display": 100, "remaining": 99, "exp": 1}
         await ctx.send(f'Level is: **{member["level"]}** Exp To Next Level Is: **{member["total_exp_next_display"]}** Remaining Exp is: **{member["remaining"]}** Your Exp Is **{member["exp"]}**')
 
+        percent = member["exp"] / member["total_exp_next_display"]
+        percent = percent * 100
+        percent = round(percent)
+        percent = int(percent)
+        percent = str(percent)
+        percent = int(percent)
+        #if user_id == 416617058248425473:
+        #    background_image = Image.open("/root/drive/Development/Sabre/cogs/other/bridge.png")
+        #else:
+        background_image = Image.open("other/haloween.png")
+        background_image = background_image.convert('RGBA') # add channel ALPHA to draw transparent rectangle
+#            image = Image.open('Cockatoo-min.png')
+        AVATAR_SIZE = 256
+
+        # --- duplicate image ----
+
+        image = background_image.copy()
+
+        image_width, image_height = image.size
+
+        # --- draw on image ---
+
+        # create object for drawing
+        
+
+        # draw red rectangle with alpha channel on new image (with the same size as original image)
+        rect_x0 = 20  # left marign
+        rect_y0 = 20  # top marign
+
+
+        draw = ImageDraw.Draw(image)
+
+# CREATE YOUR FILL COLOUR HERE (CHANGE TO 245 AT END OF EVENT)
+        color=(202,98,45)
+# Draw circle at right end of progress bar
+        x, y, diam = 330, 406, 49
+        draw.ellipse([x,y,x+diam,y+diam], fill=color)
+        percent = percent + 1
+        for iteration in range(percent):
+            if iteration < 76:
+                x = x + 11
+            elif iteration < 100:
+                x = x + 13
+            else:
+                x = x + 21
+        draw.line((350, 430) + (x, 430), fill=color, width=50)
+        if percent == 101:
+            x, y, diam = 1480, 406, 49
+            draw.ellipse([x,y,x+diam,y+diam], fill=color)
+# Flood-fill from extreme left of progress bar area to behind circle
+            # create object for drawing
+ # create new object for drawing after changing original `image`
+
+        # draw text in center
+
+
+        #font configs
+        path = "other/fonts/Quicksand-Medium.ttf"
+        font_60 = ImageFont.truetype(path, 60)
+        font_50 = ImageFont.truetype(path, 50)
+        font_75 = ImageFont.truetype(path, 75)
+        font_90 = ImageFont.truetype(path, 90)
+
+        text_width_name, text_height_name = draw.textsize(str(author.name), font=font_75)
+        x = 300     # skip avatar when center text
+        y = 315
+        
+
+        draw.text((x, y), str(author.name), fill=(255,255,255, 255), font=font_75)
+        
+
+        draw.text((x + text_width_name, y - 10), f"#{author.discriminator}", fill=(149,149,149, 255), font=font_50)
+
+
+        # draw text in center
+
+
+        text_width_exp, text_height = draw.textsize(str(member["exp"]), font=font_60)
+        x = 1300     # skip avatar when center text
+        y = 325
+        
+
+        draw.text((x, y), str(member["exp"]), fill=(255,255,255, 255), font=font_60)
+        
+        draw.text((x + text_width_exp, y), " / " + str(member["total_exp_next_display"]), fill=(149,149,149, 255), font=font_60)
+
+ # create new object for drawing after changing original `image`
+
+        # draw text in center
+        
+
+        x = 1105 + AVATAR_SIZE
+        y = 150
+        draw.text((x, y), "Level", fill=(149,149,149, 255), font=font_60)
+
+
+        text_width_level, text_height_level = draw.textsize("Level", font=font_75)
+        draw.text((x + text_width_level, y - 30), str(member["level"]), fill=(255,255,255, 255), font=font_90)
+        # --- avatar ---
+        # get URL to avatar
+        # sometimes `size=` doesn't gives me image in expected size so later I use `resize()`
+        avatar_asset = author.avatar_url_as(format='jpg', size=AVATAR_SIZE)
+
+        # read JPG from server to buffer (file-like object)
+        buffer_avatar = io.BytesIO(await avatar_asset.read())
+
+
+        # read JPG from buffer to Image
+        avatar_image = Image.open(buffer_avatar)
+
+        # resize it
+        avatar_image = avatar_image.resize((AVATAR_SIZE, AVATAR_SIZE)) #
+        circle_image = Image.new('L', (AVATAR_SIZE, AVATAR_SIZE))
+        circle_draw = ImageDraw.Draw(circle_image)
+        circle_draw.ellipse((0, 0, AVATAR_SIZE, AVATAR_SIZE), fill=255)
+        image.paste(avatar_image, (rect_x0, rect_y0), circle_image)
+
+        buffer_output = io.BytesIO()
+        image.save(buffer_output, format='PNG')
+
+        # move to beginning of buffer so `send()` it will read from beginning
+        buffer_output.seek(0)
+
+        # send image
+        await ctx.send(file=File(buffer_output, 'image.png'))
 
 
 
