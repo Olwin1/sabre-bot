e791632f1073aa8fa46a83e03e44d170d4d5c952
Olwin1 <oliver_munn@icloud.com>
2021-10-18 17:30:59 +0100

    Cleaned Up Code & Named Rank Command

--- cogs/leveling.py
+++ cogs/leveling.py
@@ -51,18 +51,16 @@ class Slash(commands.Cog):
         self.bot = bot
         
     def __del__(self):
-        print("Clearing Cache")
+        print("Clearing Cache...")
         for i in range(cache.currsize):
-            print(i)
             cache.popitem()
-        print("Cache Has Been Emptied")
+        print("Cache Has Been Emptied!")
 
 
-    @commands.command(name="cogt")
-    async def _cogt(self, ctx):
 
-    #@cog_ext.cog_slash(name="cogt", guild_ids=guild_ids)
-    #async def _cogt(self, ctx: SlashContext):
+
+    @cog_ext.cog_slash(name="rank", guild_ids=guild_ids)
+    async def _rank(self, ctx: SlashContext):
 
         author = ctx.author
         user = getcache_leveling(author.id, ctx.guild.id)
@@ -86,7 +84,7 @@ class Slash(commands.Cog):
                 total_exp_next_display = x
                 total_exp_next_actual = y
                 remaining = total_exp_next_actual - total_exp
-                print("final", total_exp_next_display, level, total_exp_next_actual, remaining)
+
 
                 member = {"guild": user["guilds"][i], "level": level, "total_exp": total_exp, "total_exp_next_actual": total_exp_next_actual, "total_exp_next_display": total_exp_next_display, "remaining": remaining, "exp": total_exp_next_display - remaining}
                 #guild, level, total_exp, total_exp_next_actual, total_exp_next_display, remaining
@@ -96,14 +94,13 @@ class Slash(commands.Cog):
             cache[author.id][1].append(1)
             print(cache[author.id][0])
             member = {"guild": ctx.guild.id, "level": 1, "total_exp": 1, "total_exp_next_actual": 100, "total_exp_next_display": 100, "remaining": 99, "exp": 1}
-        await ctx.send(f'Level is: **{member["level"]}** Exp To Next Level Is: **{member["total_exp_next_display"]}** Remaining Exp is: **{member["remaining"]}** Your Exp Is **{member["exp"]}**')
+        #await ctx.send(f'Level is: **{member["level"]}** Exp To Next Level Is: **{member["total_exp_next_display"]}** Remaining Exp is: **{member["remaining"]}** Your Exp Is **{member["exp"]}**')
 
         percent = member["exp"] / member["total_exp_next_display"]
         percent = percent * 100
         percent = round(percent)
         percent = int(percent)
-        percent = str(percent)
-        percent = int(percent)
+
         #if user_id == 416617058248425473:
         #    background_image = Image.open("/root/drive/Development/Sabre/cogs/other/bridge.png")
         #else:
@@ -120,19 +117,12 @@ class Slash(commands.Cog):
 
         # --- draw on image ---
 
-        # create object for drawing
-        
-
-        # draw red rectangle with alpha channel on new image (with the same size as original image)
-        rect_x0 = 20  # left marign
-        rect_y0 = 20  # top marign
-
 
         draw = ImageDraw.Draw(image)
 
 # CREATE YOUR FILL COLOUR HERE (CHANGE TO 245 AT END OF EVENT)
         color=(202,98,45)
-# Draw circle at right end of progress bar
+# Draw circle at left of progress bar to round
         x, y, diam = 330, 406, 49
         draw.ellipse([x,y,x+diam,y+diam], fill=color)
         percent = percent + 1
@@ -143,26 +133,27 @@ class Slash(commands.Cog):
                 x = x + 13
             else:
                 x = x + 21
-        draw.line((350, 430) + (x, 430), fill=color, width=50)
-        if percent == 101:
+        draw.line((350, 430) + (x, 430), fill=color, width=50)# Draw The Bar
+        if percent == 101:# If Max Reached Add Circle At End To Round The Bar Off
             x, y, diam = 1480, 406, 49
             draw.ellipse([x,y,x+diam,y+diam], fill=color)
-# Flood-fill from extreme left of progress bar area to behind circle
-            # create object for drawing
- # create new object for drawing after changing original `image`
-
-        # draw text in center
-
-
-        #font configs
+            
+        #Configuration For Fonts
         path = "other/fonts/Quicksand-Medium.ttf"
         font_60 = ImageFont.truetype(path, 60)
         font_50 = ImageFont.truetype(path, 50)
         font_75 = ImageFont.truetype(path, 75)
         font_90 = ImageFont.truetype(path, 90)
+        
+        
+        # ------ DRAWING OF TEXT ON IMAGE ------
 
+
+
+        # Draw User's Name
+        
         text_width_name, text_height_name = draw.textsize(str(author.name), font=font_75)
-        x = 300     # skip avatar when center text
+        x = 300
         y = 315
         
 
@@ -172,11 +163,10 @@ class Slash(commands.Cog):
         draw.text((x + text_width_name, y - 10), f"#{author.discriminator}", fill=(149,149,149, 255), font=font_50)
 
 
-        # draw text in center
-
+        # Draw User's Exp
 
         text_width_exp, text_height = draw.textsize(str(member["exp"]), font=font_60)
-        x = 1300     # skip avatar when center text
+        x = 1300
         y = 325
         
 
@@ -184,11 +174,8 @@ class Slash(commands.Cog):
         
         draw.text((x + text_width_exp, y), " / " + str(member["total_exp_next_display"]), fill=(149,149,149, 255), font=font_60)
 
- # create new object for drawing after changing original `image`
 
-        # draw text in center
-        
-
+        # Draw User's Level
         x = 1105 + AVATAR_SIZE
         y = 150
         draw.text((x, y), "Level", fill=(149,149,149, 255), font=font_60)
@@ -196,6 +183,8 @@ class Slash(commands.Cog):
 
         text_width_level, text_height_level = draw.textsize("Level", font=font_75)
         draw.text((x + text_width_level, y - 30), str(member["level"]), fill=(255,255,255, 255), font=font_90)
+        
+        
         # --- avatar ---
         # get URL to avatar
         # sometimes `size=` doesn't gives me image in expected size so later I use `resize()`
@@ -213,7 +202,7 @@ class Slash(commands.Cog):
         circle_image = Image.new('L', (AVATAR_SIZE, AVATAR_SIZE))
         circle_draw = ImageDraw.Draw(circle_image)
         circle_draw.ellipse((0, 0, AVATAR_SIZE, AVATAR_SIZE), fill=255)
-        image.paste(avatar_image, (rect_x0, rect_y0), circle_image)
+        image.paste(avatar_image, (20, 20), circle_image)
 
         buffer_output = io.BytesIO()
         image.save(buffer_output, format='PNG')
@@ -254,18 +243,18 @@ class Slash(commands.Cog):
                 total_exp_next_display = x
                 total_exp_next_actual = y
                 remaining = total_exp_next_actual - total_exp
-                print("final", total_exp_next_display, level, total_exp_next_actual, remaining)
+
 
                 member = {"guild": user["guilds"][i], "level": level, "total_exp": total_exp, "total_exp_next_actual": total_exp_next_actual, "total_exp_next_display": total_exp_next_display, "remaining": remaining, "exp": total_exp_next_display - remaining}
                 #guild, level, total_exp, total_exp_next_actual, total_exp_next_display, remaining
         if not passed:
-            print(cache[message.author.id][0])
+
             cache[message.author.id][0].append(message.guild.id)
             cache[message.author.id][1].append(1)
             for i in range(len(cache[message.author.id][0])):
                 if cache[message.author.id[0][i]] == message.guild.id:
                     iteration = i
-            print(cache[message.author.id][0])
+
             member = {"guild": message.guild.id, "level": 1, "total_exp": 1, "total_exp_next_actual": 100, "total_exp_next_display": 100, "remaining": 99, "exp": 1}
         x = cooldown.get(message.author.id)
         if x is not None:
@@ -311,7 +300,7 @@ class Slash(commands.Cog):
         for i in range(cache.currsize):
             print(i)
             cache.popitem()
-        print("Popped")
+        await ctx.send("Cache Has Been Emptied")
 
 
     @cog_ext.cog_slash(name="cache", guild_ids=guild_ids)
